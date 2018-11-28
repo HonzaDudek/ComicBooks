a2ad6bd90f254376db67eabb3d65b9356f1d4f33
Honza Dudek <honza@honzadudek.cz>
2018-11-28 20:59:12 +0100

    Added ComicBooksController

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
