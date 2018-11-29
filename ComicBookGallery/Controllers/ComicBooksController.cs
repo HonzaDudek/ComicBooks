cd8bf45e79116a1392d686cea3013c2db17b0f35
Honza Dudek <honza@honzadudek.cz>
2018-11-29 20:44:41 +0100

    Added ComicBook repository

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ComicBookGallery.Models;
+using ComicBookGallery.Repositories;
 
 namespace ComicBookGallery.Controllers
 {
@@ -12,23 +13,9 @@ namespace ComicBookGallery.Controllers
         // GET
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHTML = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, " +
-                                  "last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist("Script", "Dan Slott"),
-                    new Artist("Pencils", "Humberto Ramos"),
-                    new Artist("Inks", "Victor Olazaba"),
-                    new Artist("Colors", "Edgar Delgado"),
-                    new Artist("Letters", "Chris Eliopoulos")
-                }
-            };
+           ComicBookRepository repo = new ComicBookRepository();
 
-            return View(comicBook);
+            return View(repo.getComicBook(1));
         }
     }
 }
---
+++ ComicBookGallery/Repositories/ComicBookRepository.cs
@@ -0,0 +1,74 @@
+﻿using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Repositories
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks =
+        {
+            new ComicBook
+            {
+                Id = 1,
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHTML = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, " +
+                                  "last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new[]
+                {
+                    new Artist("Script", "Dan Slott"),
+                    new Artist("Pencils", "Humberto Ramos"),
+                    new Artist("Inks", "Victor Olazaba"),
+                    new Artist("Colors", "Edgar Delgado"),
+                    new Artist("Letters", "Chris Eliopoulos")
+                }
+            },
+            new ComicBook
+            {
+                Id = 2,
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHTML =
+                    "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Artists = new[]
+                {
+                    new Artist("Script", "Dan Slott"),
+                    new Artist("Pencils", "Marcos Martin"),
+                    new Artist("Inks", "Marcos Martin"),
+                    new Artist("Colors", "Muntsa Vicente"),
+                    new Artist("Letters", "Joe Caramagna")
+                },
+                Favorite = false
+            },
+            new ComicBook
+            {
+                Id = 3,
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHTML =
+                    "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and " +
+                    "the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious " +
+                    "\"Crown of Horns\".</p>",
+                Artists = new[]
+                {
+                    new Artist("Script", "Jeff Smith"),
+                    new Artist("Pencils", "Jeff Smith"),
+                    new Artist("Inks", "Jeff Smith"),
+                    new Artist("Letters", "Jeff Smith")
+                },
+                Favorite = false
+            }
+        };
+
+        public ComicBook getComicBook(int Id)
+        {
+            if (Id < _comicBooks.Length)
+            {
+                return _comicBooks[Id];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
